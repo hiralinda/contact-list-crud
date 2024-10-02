fc3c29f6e29e4350b03904164c858fc2d38ea648
hiralinda <lilirezerata@gmail.com>
2024-10-02 12:13:04 -0300

    create views

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Contact
+from .forms import ContactForm
 
-# Create your views here.
+# List all contacts
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contacts/contact_list.html', {'contacts': contacts})
+
+# Create a new contact
+def contact_create(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')
+    else:
+        form = ContactForm()
+    return render(request, 'contacts/contact_form.html', {'form': form})
+
+# Update an existing contact
+def contact_update(request, id):
+    contact = get_object_or_404(Contact, id=id)
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'contacts/contact_form.html', {'form': form})
+
+# Delete a contact
+def contact_delete(request, id):
+    contact = get_object_or_404(Contact, id=id)
+    if request.method == "POST":
+        contact.delete()
+        return redirect('contact_list')
+    return render(request, 'contacts/contact_confirm_delete.html', {'contact': contact})
