a3cc80fbc5eea3adc6cf3315a912405d6fabfec2
hiralinda <lilirezerata@gmail.com>
2024-10-02 21:46:10 -0300

    add style class

--- contacts/forms.py
+++ contacts/forms.py
@@ -5,3 +5,17 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'phone']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'mt-3 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm',
+                'placeholder': 'Enter name'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'mt-3 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm',
+                'placeholder': 'Enter email'
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'mt-3 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm',
+                'placeholder': 'Enter phone'
+            }),
+        }
